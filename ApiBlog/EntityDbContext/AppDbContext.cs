e4f04e74407d5a426c928d2a59608f45a8ba9b90
Dario Ramirez <dframirez80@gmail.com>
2021-10-22 16:34:45 -0300

    Fluent Api

---
+++ ApiBlog/EntityDbContext/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiBlog.Entities;
+using ApiBlog.EntityConfigurations;
+
+namespace ApiBlog.EntityDbContext
+{
+    public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions options) : base(options) {}
+		public DbSet<User> Users { get; set; }
+		public DbSet<Articule> Articules { get; set; }
+		public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new ArticuleConfiguration());
+            modelBuilder.ApplyConfiguration(new CommentConfiguration());
+        }
+    }
+
+}
--- ApiBlog/Startup.cs
+++ ApiBlog/Startup.cs
@@ -1,4 +1,4 @@
-using ApiBlog.Services;
+using ApiBlog.EntityDbContext;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
